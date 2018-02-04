7015fd7e4621364ce802d7f1a34540744831450a
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2018-02-04 21:22:30 +0300

    Add Simple Header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="Header">
+          <h1 className="Header-Title">RegExpLearn</h1>
+          <p className="Header-Intro">Тренажер регулярных выражений на JavaScript</p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
