069a8f7b86f1ea56796157f3b4539ce36577e4e8
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2018-02-07 20:34:01 +0300

    add Paper components

--- src/components/Lesson/Lesson.js
+++ src/components/Lesson/Lesson.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import TextField from 'material-ui/TextField'
+import Paper from 'material-ui/Paper'
+import './Lesson.css'
 
 export default class Lesson extends React.Component {
   render() {
     return(
       <section className='Lesson'>
-        <TextField
-          hintText="RegExp"
-        />
+        <Paper zDepth={1} className='Paper'>
+          <TextField
+            className='Paper-TextField'
+            hintText="RegExp"
+            style={{
+              width: '50vw',
+            }}
+          />
+        </Paper>
       </section>
     )
   }
